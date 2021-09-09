d07c9d3377c5eed63d13c87289300baac697f006
Ignjat Stjepanovic <53408313+Phalanxz@users.noreply.github.com>
2021-09-09 17:52:10 +0200

    Completed Javascript and CSS styling. Minor changes still to do.

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 function addNewElement() {
   let ulList = document.getElementById("myList");
-  let li = document.createElement('li');
-  var input = document.getElementById('input');  
+  let li = document.createElement("li");
+  var input = document.getElementById("input");
   li.appendChild(document.createTextNode(input.value));
   ulList.appendChild(li);
   input.value = "";
+
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "&#10060;";
+  li.appendChild(deleteButton);
+  deleteButton.addEventListener("click", function () {
+    this.parentNode.parentNode.removeChild(this.parentNode);
+  });
+
+  let doneButton = document.createElement("button");
+  doneButton.innerHTML = "&#9989";
+  li.appendChild(doneButton);
+  doneButton.addEventListener("click", function () {
+    this.parentNode.style.backgroundColor = "chartreuse";
+  });
+
+
+
 }
